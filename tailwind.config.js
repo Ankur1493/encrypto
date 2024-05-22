0fbf5b78f6334558d54c07efc91195563dde769b
Ankur1493 <ankursharma1493@gmail.com>
2024-05-22 13:22:17 +0530

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const { fontFamily } = require("tailwindcss/defaultTheme")
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+    },
+  },
+}
+

f51510863cb0650f3473d9ed644cfb34c700c9e9
Ankur1493 <ankursharma1493@gmail.com>
2024-05-22 20:01:32 +0530

    registeration actions and schema

---
+++ actions/login.ts
@@ -0,0 +1,12 @@
+"use server";
+import * as z from "zod"
+import { LoginSchema } from "@/schemas";
+
+export const login = async (values: z.infer<typeof LoginSchema>) => {
+  const validatedFields = LoginSchema.safeParse(values);
+  if (!validatedFields.success) {
+    return { error: "Invalid Fields" }
+  }
+  return { success: "User logged in" }
+
+}
--- schemas/index.ts
+++ schemas/index.ts
@@ -4,3 +4,9 @@ export const LoginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(1, { message: "password is empty" })
 })
+
+export const RegisterSchema = z.object({
+  name: z.string().min(4, { message: "minimum 4 characters required" }),
+  email: z.string().email(),
+  password: z.string().min(5, { message: "minimum 5 characters required" })
+})

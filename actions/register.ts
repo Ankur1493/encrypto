2660dba04c72e79336c1de57726ba903ace796af
Ankur1493 <ankursharma1493@gmail.com>
2024-05-22 23:52:55 +0530

    feat: registering user

--- actions/register.ts
+++ actions/register.ts
@@ -1,12 +1,26 @@
 "use server";
 import * as z from "zod"
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
   if (!validatedFields.success) {
     return { error: "Invalid Fields" }
   }
-  return { success: "User logged in" }
+  const { name, email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 14);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "email already in use" }
+  }
+
+  await db.user.create({ data: { name, email, password: hashedPassword } })
+
+  return { success: "User Created" }
 
 }
--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react"
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="h-full flex items-center justify-center">
+    <div className="h-full flex items-center justify-center drop-shadow-lg">
       {children}
     </div>
   )

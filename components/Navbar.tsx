0736ba0e10335058bcb709722a2a2fd370b0ac77
Ankur1493 <ankursharma1493@gmail.com>
2024-05-23 11:41:18 +0530

    navbar login button, dynamically rendered

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link"
 import { Button } from "./ui/button"
 import { Github } from "lucide-react"
+import { auth } from "@/auth"
 
-export default function Navbar() {
+export default async function Navbar() {
+
+  const session = await auth();
   return (
     <div className="h-16 px-16 flex justify-between items-center shadow-md shadow-gray-400">
       <div className="text-2xl font-bold text-yellow-800">
@@ -11,9 +14,12 @@ export default function Navbar() {
         </Link>
       </div>
       <div className="flex">
-        <Link href={"/auth/login"}>
-          <Button className="bg-black text-white mr-4">Login</Button>
-        </Link>
+        {
+          !session &&
+          <Link href={"/auth/login"}>
+            <Button className="bg-black text-white mr-4">Login</Button>
+          </Link>
+        }
         <Link href={"https://github.com/ankur1493/encrypto"} target="_blank">
           <Button className="flex border font-semibold bg-gradient-to-r from-pink-600 to-red-400 text-white"> <Github /> Star us</Button>
         </Link>
